56692b6c092038e1f259bb8ed24df5b6fcaaa0e4
Yash Pungaliya <yashpungaliya@gmail.com>
2017-09-29 14:06:01 +0530

    Fix imports in drivers

--- driver_headers_mbox.py
+++ driver_headers_mbox.py
@@ -2,6 +2,8 @@ import os.path
 
 from lib.input.check_headers import *
 from lib.input.mbox.mbox_hdr import extract_mail_header
+from lib.analysis.thread.graph.edge_list import *
+from lib.input.data_cleanup import *
 
 mailbox_list = [d for d in os.listdir('data') if os.path.isdir(os.path.join('data', d))]
 mailbox_list = ['lkml', 'opensuse', 'opensuse-bugs', 'opensuse-factory', 'opensuse-features', 'opensuse-kernel', 'sakai-devel']
@@ -18,11 +20,11 @@ for mailbox in mailbox_list:
     author_uid_filename = './data/' + mailbox + '/json/author_uid_map.json'
 
     print("Processing Mailbox:", mailbox)
-    extract_mail_header(mbox_filename=mbox_filename, json_filename=unclean_headers_filename,
-                        thread_uid_filename=thread_uid_filename, author_uid_filename=author_uid_filename)
-    last_uid = check_validity(False, json_header_filename=unclean_headers_filename)
-    print("Last valid UID in JSON file:", last_uid)
+    #extract_mail_header(mbox_filename=mbox_filename, json_filename=unclean_headers_filename,
+     #                   thread_uid_filename=thread_uid_filename, author_uid_filename=author_uid_filename)
+    #last_uid = check_validity(False, json_header_filename=unclean_headers_filename)
+    #print("Last valid UID in JSON file:", last_uid)
     #remove_duplicate_headers(json_header_filename=unclean_headers_filename)
-    #remove_invalid_references(input_json_filename=unclean_headers_filename, output_json_filename=headers_filename, ref_toggle=True)
-    #generate_edge_list(nodelist_filename=nodelist_filename, edgelist_filename=edgelist_filename, json_filename=unclean_headers_filename)
+    #remove_invalid_references(input_json_filename=unclean_headers_filename, output_json_filename=clean_headers_filename, ref_toggle=True)
+    generate_edge_list(nodelist_filename=nodelist_filename, edgelist_filename=edgelist_filename, json_filename=unclean_headers_filename)
     print("----------------")
